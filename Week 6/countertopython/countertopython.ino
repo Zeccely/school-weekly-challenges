7d80168e6bcd7276697aa70271b8b4452dbd0609
Zeccely <jepi.sama@gmail.com>
2024-10-07 11:11:51 +0200

    Changed some stuff
    organizing

---
+++ Week 6/countertopython/countertopython.ino
@@ -0,0 +1,21 @@
+#include <ArduinoJson.h>
+
+void setup() {
+  Serial.begin(9600);          // Start serial communication at 9600 baud
+}
+
+void loop() {
+  // Create a StaticJsonDocument object
+  StaticJsonDocument<200> doc;
+
+  // Add key-value pairs to the document
+  doc["lightLevel"] = 777;
+  doc["time"] = "9 dec 2024";
+
+  // Serialize the JSON document to a string
+  String jsonData;
+  serializeJson(doc, jsonData);
+
+  // Print the JSON string to the Serial Monitor
+  Serial.println(jsonData);
+}
