e1c884d4554ba7ca23a52cc63021ca88b9b9dac9
Zeccely <jepi.sama@gmail.com>
2024-10-15 22:17:53 +0200

    fine tuning

--- Week 6/Greenhouse post things/greenhouse_testing.py
+++ Week 6/Greenhouse post things/greenhouse_testing.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from flask import Flask
 from flask import render_template
 import random
-from flask import request, redirect
+from flask import request, redirect   #ended up not using redirect because it would make a loop and my template would not reach the rendering stage
 import serial 
 import requests
 import json
@@ -23,17 +23,17 @@ def get_time():         #this is a function that shows the time
 def get_info():         #step to define get route
     current_time = get_time_day()   #this variable will go onto flask template.
     timeonly = get_time()           # same here
-    data = json.loads(ser.readline().decode().strip())  #the data variable for template needs to be a json file. read the serial input decode strip and jsonify it
-    data["Time"]=timeonly           #this is necessary to make timestamps on the template. in data there is a time in template relate it to timeonly variable
+    data = json.loads(ser.readline().decode().strip())  #the data variable for template needs to be a json file. read the serial input decode strip and jsonify it. without json loads it becomes strings! bad!
+    data["Time"]=timeonly           #this is necessary to make timestamps on the template. in data there is a time placeholder in template. relate it to timeonly variable
     requests.post("http://127.0.0.1:5000/addData", json = data ) #post the json data 
     
-    return render_template ("exercise1.html", time = timeonly, time_day= current_time, test=infolist ) #fisplay all the data with the placeholders and the loop. test is where the jsonfiles go
+    return render_template ("exercise1.html", time = timeonly, time_day= current_time, test=infolist ) #display all the data with the placeholders and the loop. test is where the jsonfiles go
 
 @app.route("/addData", methods=["POST"]) #defining post route
 def post_info():
     thing=request.get_json()    #make a thing. this thing gets the json
     print(thing)                #debugger
-    infolist.append(thing)      #add thing into that empty list. I need it for the template
+    infolist.append(thing)      #add thing(jsonfiles) into that empty list. I need it for the template
     print(infolist)             #debugger!
     return "", 204              # I found a way for the website to respond because I had many issues with rendering the template. at some point the code wouldn't render at all
 
