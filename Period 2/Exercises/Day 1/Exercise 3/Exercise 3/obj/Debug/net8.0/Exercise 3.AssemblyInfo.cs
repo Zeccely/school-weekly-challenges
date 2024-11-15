5db74a109b37e402f1b2a16c63d1d745f6c05259
Zeccely <jepi.sama@gmail.com>
2024-11-15 16:03:58 +0100

    classes

--- Period 2/Exercises/Day 1/Exercise 3/Exercise 3/obj/Debug/net8.0/Exercise 3.AssemblyInfo.cs
+++ Period 2/Exercises/Day 1/Exercise 3/Exercise 3/obj/Debug/net8.0/Exercise 3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise 3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80b1331b2db3b65de18513e35b61071f6f456e5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a047afad73aeb9272189addeb0066d2d11fa2bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise 3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise 3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Period 2/Exercises/Day 1/Exercise 4/Exercise 4/Program.cs
@@ -0,0 +1,17 @@
+﻿List<double> numberList = new List<double> { };
+var userNumber = true;
+
+Console.WriteLine("Please Input Number");
+while (userNumber)
+{
+    double inputtedNumber = Convert.ToDouble(Console.ReadLine());
+    numberList.Add(inputtedNumber);
+    if (inputtedNumber = " ")
+    {
+        break;
+    }
+      
+}
+
+
+
