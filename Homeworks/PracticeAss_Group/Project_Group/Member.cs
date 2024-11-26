46cd16bfe2b92580360402c5fa97165e92120310
Zeccely <jepi.sama@gmail.com>
2024-11-26 11:19:35 +0100

    homeworks

---
+++ Homeworks/PracticeAss_Group/Project_Group/Member.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Group
+{
+    public class Member
+    {
+
+        public int studentID = 0;
+        private string Name;
+
+        public Member(string name)
+        {
+            this.Name = name;
+            
+        }
+        public Member(string name, int studentID)
+        {
+            this.Name = name;
+            this.studentID = studentID;
+        }
+        public string GetInfo()
+        {
+            string info;
+            if (this.studentID != 0)
+            {
+                info = $"{this.Name} NR : {studentID}";
+                return info;
+            }
+            else
+            {
+                info = this.Name;
+                return info;
+            }
+        }
+    }
+}
---
+++ Homeworks/PracticeAss_Group/Project_Group/ProjectGroup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Group
+{
+    
+    public class Project_Group
+    {
+        private string Name;
+        private List<Member> MemberList = new List<Member>();
+        string info;
+
+        public Project_Group(string name)
+        {
+            this.Name = name;
+        }
+        public string AddName()
+        { 
+            return Name; 
+        }
+        public string GetInfo(string name)
+        {
+            string infononr;
+            if (this.Name == name)
+            {
+                infononr = $"{this.Name}: No student number provided";
+                return infononr;
+            }
+            else
+            {
+                return infononr = string.Empty;
+            }
+        }
+        public string GetInfo(string name, int studentID)
+        {
+            
+            foreach (Member item in MemberList)
+            {
+               string info = $"{item.GetInfo()}";
+               
+            }
+            return info;
+        }
+    }
+
+
+}
+
