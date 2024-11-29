eb748c9d8be4cd7f6973379c0d0a695c52d18a4d
Zeccely <jepi.sama@gmail.com>
2024-11-29 15:43:56 +0100

    homework

--- Homeworks/ClassExampleSerialization/ClassExampleSerialization/obj/Debug/net8.0-windows/ClassExampleSerialization.AssemblyInfo.cs
+++ Homeworks/ClassExampleSerialization/ClassExampleSerialization/obj/Debug/net8.0-windows/ClassExampleSerialization.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Lottery")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ClassExampleSerialization")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+146c1955e808a52e9be6944682be626883aaecd4")]
-[assembly: System.Reflection.AssemblyProductAttribute("Lottery")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Lottery")]
+[assembly: System.Reflection.AssemblyProductAttribute("ClassExampleSerialization")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ClassExampleSerialization")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
---
+++ Homeworks/Lottery/Lottery/Lottery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery
+{
+    internal class Lottery
+    {
+        public int maxValue;
+        public int nrLength;
+        public Array numbas = new Array[100];
+        
+
+        public Lottery(int maxval, int nrlength) 
+        {
+            this.maxValue = maxval;
+            this.nrLength = nrlength;
+        }
+        public int DrawAllNumbers(int nrlength) 
+        {
+            foreach (int i in numbas)
+            {
+                Array drawnnumber = new Array[i];
+            }
+            
+        }
+
+    }
+}
+
