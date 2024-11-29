146c1955e808a52e9be6944682be626883aaecd4
Zeccely <jepi.sama@gmail.com>
2024-11-29 13:17:09 +0100

    homeworks

--- Homeworks/Lottery/Lottery/obj/Debug/net8.0-windows/Lottery.AssemblyInfo.cs
+++ Homeworks/Lottery/Lottery/obj/Debug/net8.0-windows/Lottery.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("StreamingMusicApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Lottery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc0a43e5ea25af2b5fbac4c3bd8fb85fd40418c0")]
-[assembly: System.Reflection.AssemblyProductAttribute("StreamingMusicApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("StreamingMusicApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("Lottery")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Lottery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
