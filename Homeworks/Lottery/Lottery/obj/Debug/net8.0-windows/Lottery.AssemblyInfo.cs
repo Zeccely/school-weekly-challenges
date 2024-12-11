e613a886de0b4476f61c8fb0c29eb4d77cf50ee5
Zeccely <jepi.sama@gmail.com>
2024-12-11 11:11:38 +0100

    it didnt work

--- Homeworks/Lottery/Lottery/obj/Debug/net8.0-windows/Lottery.AssemblyInfo.cs
+++ Homeworks/Lottery/Lottery/obj/Debug/net8.0-windows/Lottery.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lottery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b2499e72999d5a0c69e416c5771a4d3906a09e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9934999513fa768e5a9d64487ceb5d3489babdfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lottery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lottery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
