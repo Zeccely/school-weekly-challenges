875ccbf674a2ebe8584c8b799756eabbf42e548e
Zeccely <jepi.sama@gmail.com>
2025-03-11 16:36:08 +0100

    homeworks

---
+++ Season 2/ModelBindingExample/ModelBindingExample/Pages/Class.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ModelBindingExample.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public string? Message { get; set; }
+
+        public void OnPost(string value)
+        {
+            Message = value;
+        }
+
+    }
+}
